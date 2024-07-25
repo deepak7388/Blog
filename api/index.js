4b76c173ba28e517777bb3271f45d9f440ae035c
Deepak <deepakaryadeepakarya709@gmail.com>
2024-07-26 00:32:37 +0530

    commented everything for testing

--- api/index.js
+++ api/index.js
@@ -3,16 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-const userRouter = require("./routes/userRoutes");
-const blogRouter = require("./routes/blogRoutes");
-const user = require("./models/user");
-const blog = require("./models/blog");
-const cookieParser = require("cookie-parser");
+// const userRouter = require("./routes/userRoutes");
+// const blogRouter = require("./routes/blogRoutes");
+// const user = require("./models/user");
+// const blog = require("./models/blog");
+// const cookieParser = require("cookie-parser");
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(__dirname + "/uploads"));
-const uri = process.env.MONGODB_URI;
+// app.use("/uploads", express.static(__dirname + "/uploads"));
+// const uri = process.env.MONGODB_URI;
 
 // mongoose
 //   .connect(uri)
