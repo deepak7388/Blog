f00e740800b2adcb5d3bcd077ef6b0b8fdaa8fb6
Deepak <deepakaryadeepakarya709@gmail.com>
2024-07-26 00:36:47 +0530

    uncommented everything for testing

--- api/index.js
+++ api/index.js
@@ -3,30 +3,30 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-// const userRouter = require("./routes/userRoutes");
-// const blogRouter = require("./routes/blogRoutes");
-// const user = require("./models/user");
-// const blog = require("./models/blog");
-// const cookieParser = require("cookie-parser");
-// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+const userRouter = require("./routes/userRoutes");
+const blogRouter = require("./routes/blogRoutes");
+const user = require("./models/user");
+const blog = require("./models/blog");
+const cookieParser = require("cookie-parser");
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
-// app.use("/uploads", express.static(__dirname + "/uploads"));
-// const uri = process.env.MONGODB_URI;
+app.use("/uploads", express.static(__dirname + "/uploads"));
+const uri = process.env.MONGODB_URI;
 
-// mongoose
-//   .connect(uri)
-//   .then(() => {
-app.listen(3000, () => {
-  console.log("Server started at port " + 3000);
-});
-// })
-// .catch((err) => {
-//   console.log(err);
-// });
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server started at port " + 3000);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// app.use("/user", userRouter);
-// app.use("/blog", blogRouter);
+app.use("/user", userRouter);
+app.use("/blog", blogRouter);
 // app.post('/test',(req,res)=>{
 //     const {email,username,password}=req.body;
 //     // console.log(email,username,password);
